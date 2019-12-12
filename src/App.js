f81b5bed8c5a5348a669d39c6eb6d2c8a067d591
Sylvie LeDuc <Sylvie@Sylvies-MacBook-Pro.local>
2019-12-13 02:15:35 +0800

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import { whileStatement } from '@babel/types';
 
 
 function App() {
@@ -21,40 +22,55 @@ function App() {
   }
   return (
     <div className = "App">
-      {page === "" && (
+      <div class="page">{page === "" && (
         <>
           <h1 className="title">ROUTE</h1>
-          <button onClick={scanButton}>Scan QR Code</button>
-          <input type="text" name="Enter Product Brand Name" onChange={inputBox} placeholder="Product Brand Name"/>
-          <button onClick={submitButton}>SUBMIT</button>
+          <button class="scanbutton" onClick={scanButton}>Scan QR Code</button>
+          <div class="input_button">
+            <input type="text" class ="input"name="Product Name and Location" onChange={inputBox} placeholder="Product Name"/>
+            <button class = "submitbutton" onClick={submitButton}>SUBMIT</button>
+          </div>
         </>
       )}
       {page === "Scanned" && <ButtonPage />}
       {page === "Input" && <InputPage i={input}/>}
     </div>
+    </div>
   );
 }
 
 function InputPage(props) {
-  let data = ['Barilla', 'Yoplait', 'Goya Chickpeas'];
-  let currentProduct = props.i;
+  let data = ['Barilla'];
+  let currentProduct = props.i; 
   return (
-    <div className="input_page">
-    <p>{props.i}</p>
+    <div class="page">
+      <div className="input_page">
+      <p class="props">{props.i}</p>
+      <img src="https://cdn.britannica.com/91/183591-050-34AC9668/World-Data-Locator-Map-Bangladesh.jpg" height="200"/>
+      <div class="input-text">
+        <div>Origin Location: Dhaka, Bhutan</div>
+        <div>This item traveled: 1963 miles</div>
+        <div>Carbon Emissions: 1650 kg CO2</div>
+        <div class="ORANGE">This product gets an ORANGE route rating.</div>
+      </div>
+      </div>
     </div>
   )
 }
+
 function ButtonPage() {
   return (
+  <div class="page">
   <div className="product_page">
-    <h1 className="product">Barilla Pasta</h1>
-    <img src="https://www.meijer.com/content/dam/meijer/product/0089/50/5900/06/0089505900061_1_A1C1_1200.png" height="200" ></img>
+    <h1 className="product">This item traveled: 6926 MILES</h1>
+    <img src="https://www.worldatlas.com/img/locator/city/081/21781-new-york-city-locator-map.jpg" height="200" alt="pasta box"></img>
     <div className="product_info">
-      <div>This product traveled 5,125 miles to reach your location.</div>
-      <div>Through travel alone this product emitted around 700 kg of Carbon Dioxide.</div>
-      <div>It's origin location is Copenhagen, Denmark. </div>
-      <div>This product gets a Red Route rating. We wouldn't recommend purchasing.</div>
+      <div class="a">Origin Location : Avon, NY USA</div>
+      <div class="b">Your Location: Shanghai, China</div>
+      <div class="c">Carbon Emissions: 2500 kg CO2</div>
+      <div class="RED">This product gets a RED route rating</div>
     </div>
+  </div>
   </div>
   )
 }
